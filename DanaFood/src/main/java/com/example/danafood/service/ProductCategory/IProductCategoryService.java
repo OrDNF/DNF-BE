86d824fce1fd65e0f9e2fc261e8753a7714c6474
DienTon <thatdien295@gmail.com>
2024-10-08 21:40:02 +0700

    up

--- DanaFood/src/main/java/com/example/danafood/service/ProductCategory/IProductCategoryService.java
+++ DanaFood/src/main/java/com/example/danafood/service/ProductCategory/IProductCategoryService.java
@@ -1,9 +1,17 @@
 package com.example.danafood.service.ProductCategory;
 
+import com.example.danafood.model.Product;
 import com.example.danafood.model.ProductCategory;
+import com.example.danafood.model.dto.ProductDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface IProductCategoryService {
     Optional<ProductCategory> findById(Long id);
+    Page<ProductCategory> findALlCategory(Pageable pageable);
+    void deleteCategoryById(Long id);
+    Product addNewCategory(ProductCategory category);
+    Product editCategory(Long id,ProductCategory category);
 }
--- DanaFood/src/main/java/com/example/danafood/service/ProductCategory/ProductCategoryService.java
+++ DanaFood/src/main/java/com/example/danafood/service/ProductCategory/ProductCategoryService.java
@@ -1,8 +1,12 @@
 package com.example.danafood.service.ProductCategory;
 
+import com.example.danafood.model.Product;
 import com.example.danafood.model.ProductCategory;
+import com.example.danafood.model.dto.ProductDTO;
 import com.example.danafood.repository.IProductCategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,4 +20,26 @@ public class ProductCategoryService implements IProductCategoryService {
     public Optional<ProductCategory> findById(Long id) {
         return iProductCategoryRepo.findById(id);
     }
+
+    @Override
+    public Page<ProductCategory> findALlCategory(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+
+    }
+
+    @Override
+    public Product addNewCategory(ProductCategory category) {
+        return null;
+    }
+
+    @Override
+    public Product editCategory(Long id, ProductCategory category) {
+        return null;
+    }
+
+
 }
